175854678b458f3d0e2c264ee0b4a48f6c49f719
Tilman Möller <moeller.tilman@gmail.com>
2021-06-04 12:05:22 +0200

    use corperate logo and icon

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -75,8 +75,8 @@ const Layout: React.FC<LayoutProps> = ({ children, selectedNavigation}) =>{
               <div className="flex-shrink-0 flex items-center px-4">
                 <img
                   className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/easywire-logo-cyan-300-mark-white-text.svg"
-                  alt="Easywire logo"
+                  src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Logo_HTW_Berlin.svg"
+                  alt="HTW logo"
                 />
               </div>
               <nav className="mt-5 flex-shrink-0 h-full divide-y divide-cyan-800 overflow-y-auto" aria-label="Sidebar">
@@ -126,9 +126,9 @@ const Layout: React.FC<LayoutProps> = ({ children, selectedNavigation}) =>{
           <div className="flex flex-col flex-grow bg-cyan-700 pt-5 pb-4 overflow-y-auto">
             <div className="flex items-center flex-shrink-0 px-4">
               <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/easywire-logo-cyan-300-mark-white-text.svg"
-                alt="Easywire logo"
+                className="h-8 w-auto self"
+                src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Logo_HTW_Berlin.svg"
+                alt="HTW logo"
               />
             </div>
             <nav className="mt-5 flex-1 flex flex-col divide-y divide-cyan-800 overflow-y-auto" aria-label="Sidebar">
