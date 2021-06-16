3ae1be6d1fd565170a1c2bfbcaec1be18641bb15
Tilman Möller <moeller.tilman@gmail.com>
2021-06-16 14:57:29 +0200

    a user action acts as middleware and process the user token

---
+++ backend/app/actions/UserAction.scala
@@ -0,0 +1,48 @@
+package actions
+
+import javax.inject.Inject
+import play.api.Logging
+import play.api.mvc._
+import play.api.http.HeaderNames
+import play.api.mvc.Results.Unauthorized
+import play.api.libs.json.Json
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration.Duration
+
+import models.User
+import repositories.UserRepository
+import services.TokenService
+
+
+case class UserRequest[A](user: User, request: Request[A]) extends WrappedRequest[A](request)
+
+
+class UserAction @Inject() (bodyParser: BodyParsers.Default, tokenService: TokenService, userRepository: UserRepository)(implicit ec: ExecutionContext) extends ActionBuilder[UserRequest, AnyContent] with Logging {
+
+  private val headerTokenRegex = """Bearer (.+?)""".r
+
+  override def parser: BodyParser[AnyContent] = bodyParser
+
+  override def invokeBlock[A](request: Request[A], block: UserRequest[A] => Future[Result]): Future[Result] = {
+    logger.info("UserAction - " + request)
+
+    extractBearerToken(request) match {
+      case None => Future.successful(Unauthorized("No token given"))
+      case Some(token) => tokenService.validate(token) match {
+        case Left(errors) => Future.successful(Unauthorized(Json.toJson(errors)))
+        case Right(username) => Await.result(userRepository.getByUsername(username), Duration.Inf) match {
+          case None => Future.successful(Unauthorized("Invalid issuer"))
+          case Some(user) =>  block(UserRequest(user, request))
+        }
+      }
+    }
+  }
+
+  private def extractBearerToken[A](request: Request[A]): Option[String] =
+    request.headers.get(HeaderNames.AUTHORIZATION) collect {
+      case headerTokenRegex(token) => token
+    }
+
+  override protected def executionContext: ExecutionContext = ec
+
+}
--- backend/app/controllers/HomeController.scala
+++ backend/app/controllers/HomeController.scala
@@ -3,15 +3,13 @@ package controllers
 import play.api.mvc._
 import javax.inject._
 
-import services.TokenService
+import actions.{UserAction, UserRequest}
 
 
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents, tokenService: TokenService) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents, userAction: UserAction) extends AbstractController(cc) {
 
-  def index: Action[AnyContent] = Action {
-    val a = tokenService.encode("s0558151")
-    print(a)
+  def index: Action[AnyContent] = userAction { implicit request: UserRequest[AnyContent] =>
     Ok("Ok")
   }
 
