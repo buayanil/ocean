84da01273fe2fd3e9cec8ee30798eb38a9b3e54f
Tilman Möller <moeller.tilman@gmail.com>
2021-06-16 23:53:01 +0200

    add view for sign in and use a sign form component

--- frontend/src/views/SignInView.tsx
+++ frontend/src/views/SignInView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import SignInForm from '../components/SignInForm';
 import UserLayout from '../layouts/UserLayout';
 
 
@@ -7,7 +8,14 @@ interface SignInViewProps {}
 
 const SignInView: React.FC<SignInViewProps> = () => {
   return (
-    <UserLayout>SignInView</UserLayout>
+    <UserLayout>
+      <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your HTW account</h2>
+        </div>
+        <SignInForm />
+    </div>
+  </UserLayout>
   );
 }
 
