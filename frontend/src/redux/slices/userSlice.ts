e519c1af139c3092264dfb106836ea9ccd364ffd
Tilman Möller <moeller.tilman@gmail.com>
2021-06-18 18:37:04 +0200

    remove unused import rootstate

--- frontend/src/redux/slices/userSlice.ts
+++ frontend/src/redux/slices/userSlice.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
-import type { RootState } from '../store'
 import { CrendentialProperties, UserProperties } from '../../types/models'
 
 
