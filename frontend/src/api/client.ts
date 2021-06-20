e64ffb21dfe16fa5cbeebfd963cd0168d96653d7
Tilman Möller <moeller.tilman@gmail.com>
2021-06-20 19:17:37 +0200

    split frontend enviroment configuration in prodution and development

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const { REACT_APP_API_URL } = process.env;
+
 export const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:9000/',
+    baseURL: REACT_APP_API_URL || '',
     timeout: 20000,
     headers: {
       'Content-Type': 'application/json',
