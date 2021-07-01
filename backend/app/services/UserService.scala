6787c65b2a0d6efcc4b18967272c1a21dd0cd322
Tilman Möller <moeller.tilman@gmail.com>
2021-07-01 21:27:33 +0200

    refactor: use the same insertQuery like in instances

--- backend/app/services/UserService.scala
+++ backend/app/services/UserService.scala
@@ -5,7 +5,7 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 import repositories.UserRepository
-import models.ErrorMessage
+import models.{ErrorMessage, LdapUser, User}
 
 
 class UserService @Inject()(ldapService: LdapService, tokenService: TokenService ,userRepository: UserRepository) {
@@ -16,11 +16,15 @@ class UserService @Inject()(ldapService: LdapService, tokenService: TokenService
       case Right(ldapUser) => Await.result(userRepository.getByUsername(ldapUser.username), Duration.Inf) match {
         case Some(user) => Right(tokenService.encode(user.username))
         case None => Await.result(
-          userRepository.create(ldapUser.username, ldapUser.firstName, ldapUser.lastName, ldapUser.mail, ldapUser.employeeType), Duration.Inf) match {
+          userRepository.addUser(getUserFor(ldapUser)), Duration.Inf) match {
           case user => Right(tokenService.encode(user.username))
         }
       }
     }
+  }
+
+  private def getUserFor(ldapUser: LdapUser): User = {
+    User(0L, ldapUser.username, ldapUser.firstName, ldapUser.lastName, ldapUser.mail, ldapUser.employeeType)
   }
 
 }
