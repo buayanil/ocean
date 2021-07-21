b62918b22882ab45d2c90ccc7ec2a98276b12f9a
Tilman Möller <moeller.tilman@gmail.com>
2021-07-21 15:04:46 +0200

    getting started redirects to views

---
+++ frontend/src/constants/starting.ts
@@ -0,0 +1,33 @@
+import { CogIcon, DatabaseIcon } from "@heroicons/react/outline";
+
+export interface StartingPoint {
+  title: string;
+  description: string;
+  icon: (props: React.ComponentProps<"svg">) => JSX.Element;
+  to: string;
+  background: string;
+}
+
+export const startingPoints: StartingPoint[] = [
+  {
+    title: "Create a Database",
+    description: "Add a new database now.",
+    icon: DatabaseIcon,
+    to: "databases/new",
+    background: "bg-pink-500",
+  },
+  {
+    title: "Manage Databases",
+    description: "Manage databases in one place.",
+    icon: DatabaseIcon,
+    to: "databases/",
+    background: "bg-pink-500",
+  },
+  {
+    title: "Manage your profile",
+    description: "All your user-specific settings are located here.",
+    icon: CogIcon,
+    to: "settings/",
+    background: "bg-yellow-500",
+  },
+];
--- frontend/src/views/OverviewView.tsx
+++ frontend/src/views/OverviewView.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import AppLayout from '../layouts/AppLayout';
-import { OverviewNavigation } from '../constants/menu.';
-
+import AppLayout from "../layouts/AppLayout";
+import { OverviewNavigation } from "../constants/menu.";
+import { startingPoints } from "../constants/starting";
+import Headline from "../components/Headline";
+import StartingPoints from "../components/StartingPoints";
 
 interface OverviewViewProps {}
 
+const { REACT_APP_ISSUE_LINK } = process.env;
+
 const OverviewView: React.FC<OverviewViewProps> = () => {
   return (
-    <AppLayout selectedNavigation={OverviewNavigation.name}>OverviewView</AppLayout>
+    <AppLayout selectedNavigation={OverviewNavigation.name}>
+      <Headline title="Overview" size="large" />
+      <StartingPoints
+        title="Getting started"
+        description="Get started by selecting a template."
+        startingPoints={startingPoints}
+        moreHref={REACT_APP_ISSUE_LINK || "#"}
+      />
+    </AppLayout>
   );
-}
+};
 
 export default OverviewView;
