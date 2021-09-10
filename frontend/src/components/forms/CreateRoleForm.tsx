62ec5dd8357600cd9723c732ec210f35434e5216
Tilman Möller <moeller.tilman@gmail.com>
2021-09-10 10:29:44 +0200

    fix: schema pattern for rolename

--- frontend/src/components/forms/CreateRoleForm.tsx
+++ frontend/src/components/forms/CreateRoleForm.tsx
@@ -19,6 +19,8 @@ const CreateRoleForm: React.FC<CreateRoleFormProps> = ({ database, onSubmit, onC
         roleName: yup
             .string()
             .required("Name is required")
+            .min(4, "Name should be of minimum 4 characters length")
+            .matches(/^[a-z][a-z0-9_]*$/, "Name must begin with a letter (a-z). Subsequent characters in a name can be letters, digits (0-9), or underscores.")
             .test("unique_test", "Name is already registered", (value, ctx) =>
                 validateDatabaseValues(value, ctx)
             )
