2592ce25d0f6dea54b79546046f95788ad70cb82
Tilman Möller <moeller.tilman@gmail.com>
2021-11-21 20:11:03 +0100

    feat: on startup service checks for expected roles

--- backend/app/Module.scala
+++ backend/app/Module.scala
@@ -1,6 +1,6 @@
 import com.google.inject.AbstractModule
+import com.htwhub.ocean.service.OnStartupService
 import java.time.Clock
-import services.StartupService
 
 /** This class is a Guice module that tells Guice how to bind several different types. This Guice module is created when
   * the Play application starts.
@@ -14,6 +14,6 @@ class Module extends AbstractModule {
     // Use the system clock as the default implementation of Clock
     bind(classOf[Clock]).toInstance(Clock.systemDefaultZone)
     // Use the service to check if all environment requirements are satisfied
-    bind(classOf[StartupService]).asEagerSingleton()
+    bind(classOf[OnStartupService]).asEagerSingleton()
   }
 }
