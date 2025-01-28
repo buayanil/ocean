fcc7e4fb3e02b67158e8b045d5062214bb6da981
Ahmad Fadhil <92725066+buayanil@users.noreply.github.com>
2025-01-28 11:38:27 +0100

    Implemented e2e testing for DatabaseDetailView.tsx

--- frontend/cypress/e2e/databases/DatabaseDetailView.cy.tsx
+++ frontend/cypress/e2e/databases/DatabaseDetailView.cy.tsx
@@ -3,11 +3,12 @@ describe("DatabaseDetailView Test", () => {
     const userApiUrl = "http://databases.f4.htw-berlin.de:9000/v1/user"; // User data endpoint
     const databasesApiUrl = "http://databases.f4.htw-berlin.de:9000/v1/databases"; // Get databases endpoint
     const databasesRolesApiUrl = "http://databases.f4.htw-berlin.de:9000/v1/databases/1/roles";
-    const invitationsApiUrl = "http://databases.f4.htw-berlin.de:9000/v1/databases/1/invitations";
+    const databaseInvitationsApiUrl = "http://databases.f4.htw-berlin.de:9000/v1/databases/1/invitations";
     const usersApiUrl = "http://databases.f4.htw-berlin.de:9000/v1/users";
     const database1ApiUrl = "http://databases.f4.htw-berlin.de:9000/v1/databases/1";
     const checkRolesAvailabilityApiUrl = "http://databases.f4.htw-berlin.de:9000/v1/roles/_availability_";
     const rolesApiUrl ="http://databases.f4.htw-berlin.de:9000/v1/roles"
+    const invitationsApiUrl = "http://databases.f4.htw-berlin.de:9000/v1/invitations";
 
     beforeEach(() => {
         // Intercept login API call
@@ -61,30 +62,32 @@ describe("DatabaseDetailView Test", () => {
             statusCode: 200,
             body: [
                 {
-                    id: 1,
-                    username: "TestUser",
-                    firstName: "Test",
-                    lastName: "User",
-                    mail: "testuser@example.com",
-                    employeeType: "Admin",
-                },
-                {
-                    id: 2,
-                    username: "TestUser",
-                    firstName: "Test",
-                    lastName: "User",
-                    mail: "testuser@example.com",
-                    employeeType: "Student",
-                },
+                    "id": 4,
+                    "instanceId": 4,
+                    "name": "aaaa_aaxx",
+                    "password": "4nv9teJGD"
+                }
             ],
         }).as("getRoles");
 
-        // Mock invitations
+        // Mock database invitations
         // @ts-ignore
-        cy.intercept("GET", invitationsApiUrl, {
+        cy.intercept("GET", databaseInvitationsApiUrl, {
             statusCode: 200,
-            body: [{ id: 1, userId: 2 }],
-        }).as("getInvitations");
+            body: [{
+                "id": 1,
+                "instanceId": 1,
+                "userId": 1,
+                "createdAt": 1737991327596
+                },
+                {
+                    "id": 2,
+                    "instanceId": 2,
+                    "userId": 2,
+                    "createdAt": 1737991327596
+                },
+            ],
+        }).as("getDatabaseInvitations");
 
         // Mock users
         // @ts-ignore
@@ -107,6 +110,14 @@ describe("DatabaseDetailView Test", () => {
                     mail: "otheruser@example.com",
                     employeeType: "Admin",
                 },
+                {
+                    id: 3,
+                    username: "OtherUser1",
+                    firstName: "Other1",
+                    lastName: "User1",
+                    mail: "otheruser1@example.com",
+                    employeeType: "Admin",
+                },
             ],
         }).as("getUsers");
 
@@ -124,9 +135,23 @@ describe("DatabaseDetailView Test", () => {
             });
         }).as("checkAvailability");
 
-        // Intercept availability check API call
+        // Intercept create a new Role API call
         // @ts-ignore
         cy.intercept("POST", rolesApiUrl, (req) => {
+            req.reply({
+                statusCode: 200,
+                body: {
+                    "id": 3,
+                    "instanceId": 3,
+                    "userId": 3,
+                    "createdAt": 1737991327596
+                },
+            });
+        }).as("createRole");
+
+        // Intercept create a new Invitation API call
+        // @ts-ignore
+        cy.intercept("POST", invitationsApiUrl, (req) => {
             req.reply({
                 statusCode: 200,
                 body: {
@@ -134,10 +159,195 @@ describe("DatabaseDetailView Test", () => {
                     "roleName": "new_role"
                 },
             });
-        }).as("createRole");
+        }).as("createInvitation");
     });
 
-    it("shows DatabaseList when databases exist", () => {
+    it("Fails to create a new invitation", () => {
+        // @ts-ignore
+        cy.intercept("POST", invitationsApiUrl, {
+            statusCode: 400, // Simulate good request
+            body: { message: "Failed to create invitation" },
+        }).as("createInvitationError");
+
+        // Visit the login page
+        cy.visit("http://localhost:5173/login");
+
+        // Log in with valid credentials
+        cy.get('input[name="username"]').type("testuser");
+        cy.get('input[name="password"]').type("password123");
+        cy.get('button[type="submit"]').click();
+        cy.get('a[href="/databases/"]').click();
+
+        cy.wait("@getDatabases");
+
+        // Click on the first database
+        cy.contains('p', 'Test Database 1').click();
+
+        // Wait for the API calls to complete
+        cy.wait("@getRoles");
+        cy.wait("@getDatabaseInvitations");
+        cy.wait("@getUsers");
+        // Switch to Users tab
+        cy.contains("Invitations").click();
+        cy.get('button.relative.w-full[aria-haspopup="listbox"]').click();
+        cy.contains('li', 'O. User1').click();
+        // Verify the invitation creation request
+        cy.wait("@createInvitationError").its("response.statusCode").should("eq", 400);
+        cy.contains('p', 'Something went wrong :(').should("exist");
+        cy.contains('button', 'Close').click();
+    });
+
+    it("Successfully delete an invitation", () => {
+        // @ts-ignore
+        cy.intercept("DELETE", `${invitationsApiUrl}/2`, {
+            statusCode: 200, // Simulate good request
+            body: { message: "" },
+        }).as("deleteInvitation2");
+
+        // Visit the login page
+        cy.visit("http://localhost:5173/login");
+
+        // Log in with valid credentials
+        cy.get('input[name="username"]').type("testuser");
+        cy.get('input[name="password"]').type("password123");
+        cy.get('button[type="submit"]').click();
+        cy.get('a[href="/databases/"]').click();
+
+        cy.wait("@getDatabases");
+
+        // Click on the first database
+        cy.contains('p', 'Test Database 1').click();
+
+        // Wait for the API calls to complete
+        cy.wait("@getRoles");
+        cy.wait("@getDatabaseInvitations");
+        cy.wait("@getUsers");
+
+        // Switch to Invitations tab
+        cy.contains("Invitations").click();
+
+        cy.contains('div', 'Delete').click();
+        // Verify the delete role request
+        cy.wait("@deleteInvitation2").its("response.statusCode").should("eq", 200);
+        cy.contains('p', 'Successfully delete!').should("exist");
+        cy.contains('button', 'Close').click();
+    });
+
+    it("Fails to delete an invitation", () => {
+        // @ts-ignore
+        cy.intercept("DELETE", `${invitationsApiUrl}/2`, {
+            statusCode: 400, // Simulate bad request
+            body: { message: "" },
+        }).as("deleteInvitation2");
+
+        // Visit the login page
+        cy.visit("http://localhost:5173/login");
+
+        // Log in with valid credentials
+        cy.get('input[name="username"]').type("testuser");
+        cy.get('input[name="password"]').type("password123");
+        cy.get('button[type="submit"]').click();
+        cy.get('a[href="/databases/"]').click();
+
+        cy.wait("@getDatabases");
+
+        // Click on the first database
+        cy.contains('p', 'Test Database 1').click();
+
+        // Wait for the API calls to complete
+        cy.wait("@getRoles");
+        cy.wait("@getDatabaseInvitations");
+        cy.wait("@getUsers");
+
+        // Switch to Invitations tab
+        cy.contains("Invitations").click();
+
+        cy.contains('div', 'Delete').click();
+        // Verify the delete role request
+        cy.wait("@deleteInvitation2").its("response.statusCode").should("eq", 400);
+        cy.contains('p', 'Something went wrong :(').should("exist");
+        cy.contains('button', 'Close').click();
+    });
+
+    it("Successfully delete a user", () => {
+        // @ts-ignore
+        cy.intercept("DELETE", `${rolesApiUrl}/4`, {
+            statusCode: 200, // Simulate good request
+            body: { message: "" },
+        }).as("deleteRole");
+
+        // Visit the login page
+        cy.visit("http://localhost:5173/login");
+
+        // Log in with valid credentials
+        cy.get('input[name="username"]').type("testuser");
+        cy.get('input[name="password"]').type("password123");
+        cy.get('button[type="submit"]').click();
+        cy.get('a[href="/databases/"]').click();
+
+        cy.wait("@getDatabases");
+
+        // Click on the first database
+        cy.contains('p', 'Test Database 1').click();
+
+        // Wait for the API calls to complete
+        cy.wait("@getRoles");
+        cy.wait("@getDatabaseInvitations");
+        cy.wait("@getUsers");
+
+        // Switch to Users tab
+        cy.contains("Users").click();
+
+        cy.contains('div', 'Delete').click();
+        // Verify the delete role request
+        cy.wait("@deleteRole").its("response.statusCode").should("eq", 200);
+        cy.contains('p', 'Successfully deleted!').should("exist");
+        cy.contains('button', 'Close').click();
+    });
+
+    it("Fails to delete a user", () => {
+        // @ts-ignore
+        cy.intercept("DELETE", `${rolesApiUrl}/4`, {
+            statusCode: 400, // Simulate good request
+            body: { message: "Failed to create role" },
+        }).as("deleteRole");
+
+        // Visit the login page
+        cy.visit("http://localhost:5173/login");
+
+        // Log in with valid credentials
+        cy.get('input[name="username"]').type("testuser");
+        cy.get('input[name="password"]').type("password123");
+        cy.get('button[type="submit"]').click();
+        cy.get('a[href="/databases/"]').click();
+
+        cy.wait("@getDatabases");
+
+        // Click on the first database
+        cy.contains('p', 'Test Database 1').click();
+
+        // Wait for the API calls to complete
+        cy.wait("@getRoles");
+        cy.wait("@getDatabaseInvitations");
+        cy.wait("@getUsers");
+
+        // Switch to Users tab
+        cy.contains("Users").click();
+
+        cy.contains('div', 'Delete').click();
+        // Verify the delete role request
+        cy.wait("@deleteRole").its("response.statusCode").should("eq", 400);
+        cy.contains('p', 'Something went wrong').should("exist");
+        cy.contains('button', 'Close').click();
+    });
+
+    it("Fails to create new user", () => {
+        // @ts-ignore
+        cy.intercept("POST", rolesApiUrl, {
+            statusCode: 400, // Simulate bad request
+            body: { message: "Failed to create role" },
+        }).as("createRoleError");
+
         // Visit the login page
         cy.visit("http://localhost:5173/login");
 
@@ -156,7 +366,44 @@ describe("DatabaseDetailView Test", () => {
         cy.url().should("include", "/databases/1");
         // Wait for the API calls to complete
         cy.wait("@getRoles");
-        cy.wait("@getInvitations");
+        cy.wait("@getDatabaseInvitations");
+        cy.wait("@getUsers");
+
+        // Switch to Users tab
+        cy.contains("Users").click();
+
+        // Add a new user role
+        cy.contains("Add new user").click();
+        cy.get('input[name="roleName"]').type("new_role"); // Replace with actual input name
+        cy.wait(1000)
+        cy.get('button.bg-indigo-600').contains('Create').click();
+
+        // Verify the role creation request
+        cy.wait("@createRoleError").its("response.statusCode").should("eq", 400);
+        cy.contains('p', 'Something went wrong :(').should("exist");
+        cy.contains('button', 'Close').click();
+    });
+
+    it("Login and visit the detail db view", () => {
+        // Visit the login page
+        cy.visit("http://localhost:5173/login");
+
+        // Log in with valid credentials
+        cy.get('input[name="username"]').type("testuser");
+        cy.get('input[name="password"]').type("password123");
+        cy.get('button[type="submit"]').click();
+        cy.get('a[href="/databases/"]').click();
+
+        cy.wait("@getDatabases");
+
+        // Click on the first database
+        cy.contains('p', 'Test Database 1').click();
+
+        // Verify navigation to the database details page
+        cy.url().should("include", "/databases/1");
+        // Wait for the API calls to complete
+        cy.wait("@getRoles");
+        cy.wait("@getDatabaseInvitations");
         cy.wait("@getUsers");
 
         // Verify the database details are displayed
@@ -182,7 +429,13 @@ describe("DatabaseDetailView Test", () => {
 
         // Open delete modal
         cy.contains('button', 'Actions').click();
+        cy.contains("Delete").click();
 
+        //Ensure the close button works
+        cy.contains('button', 'Close').click();
+
+        // Open delete modal again
+        cy.contains('button', 'Actions').click();
         cy.contains("Delete").click();
 
         // Confirm delete
@@ -201,12 +454,29 @@ describe("DatabaseDetailView Test", () => {
 
         // Add a new user role
         cy.contains("Add new user").click();
+
+        //Ensure the cancel button works
+        cy.contains('button', 'Cancel').click();
+
+        //Open the modal again
+        cy.contains("Add new user").click();
+
         cy.get('input[name="roleName"]').type("new_role"); // Replace with actual input name
         cy.wait(1000)
         cy.get('button.bg-indigo-600').contains('Create').click();
 
         // Verify the role creation request
         cy.wait("@createRole").its("response.statusCode").should("eq", 200);
+        cy.contains('p', 'Successfully created!').should("exist");
+        cy.contains('button', 'Close').click();
 
+        // Switch to Users tab
+        cy.contains("Invitations").click();
+        cy.get('button.relative.w-full[aria-haspopup="listbox"]').click();
+        cy.contains('li', 'O. User1').click();
+        // Verify the invitation creation request
+        cy.wait("@createInvitation").its("response.statusCode").should("eq", 200);
+        cy.contains('p', 'Successfully created!').should("exist");
+        cy.contains('button', 'Close').click();
     });
 });
